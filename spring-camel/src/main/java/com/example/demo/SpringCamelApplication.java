719f91a5f5ee350476bdfd5915cabbd8eac5b359
Joel Lobo <joellobo@gmail.com>
2021-03-17 21:12:44 -0300

    Added Apache Camel project

---
+++ spring-camel/src/main/java/com/example/demo/SpringCamelApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCamelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCamelApplication.class, args);
+	}
+
+}
