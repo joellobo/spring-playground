b142a5626cae2a1e329d04a7542d058fdfccb035
joellobo <joellobo@gmail.com>
2021-05-05 22:14:43 -0300

    Added another example

--- resttemplate/src/main/java/org/example/RestTemplateMain.java
+++ resttemplate/src/main/java/org/example/RestTemplateMain.java
@@ -7,12 +7,29 @@ public class RestTemplateMain {
 
 	public static void main(String[] args) {
 		RestTemplate restTemplate = new RestTemplate();
-
+		
+		/*
 		String fooResourceUrl = "https://pokeapi.co/api/v2/item/";
-
 		ResponseEntity<String> response = restTemplate.getForEntity(fooResourceUrl + "/1", String.class);
-
-		System.out.println(response.getStatusCode());
+		System.out.println(response);
+		*/
+		
+		String fooResourceUrl = "https://pokeapi.co/api/v2/item/";
+		ResponseEntity<Item> item = restTemplate.getForEntity(fooResourceUrl + "/1", Item.class);
+		System.out.println(item.getBody().getId());
+		
 	}
 
+	static class Item {
+		private Integer id;
+		
+		public Integer getId() {
+			return id;
+		}
+		
+		@Override
+		public String toString() {
+			return "Item[id=" + id + "]";
+		}
+	}
 }
